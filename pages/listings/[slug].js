c8f7961fac53a6810bed0de640bedddd3925288e
taher33 <taherhehe@gmail.com>
2021-07-12 20:27:31 +0100

    ....

--- pages/listings/[slug].js
+++ pages/listings/[slug].js
@@ -28,8 +28,10 @@ export async function getStaticProps(context) {
     const products = await getProducts(context.params);
     props = { ...props, products };
   } catch (error) {
-    console.log(error);
+    console.log("err", error);
+    props.products = [];
   }
+
   return {
     props,
     revalidate: 30,
@@ -38,12 +40,19 @@ export async function getStaticProps(context) {
 
 async function getProducts(params, page = 1) {
   try {
-    const { data } = await axios_instance()({
-      method: "GET",
-      url: "products?type=" + params.slug + "&page=" + page,
-    });
-
-    return data.products;
+    let response;
+    if (params.slug === "new") {
+      response = await axios_instance()({
+        method: "GET",
+        url: "products?" + "page=" + page,
+      });
+    } else {
+      response = await axios_instance()({
+        method: "GET",
+        url: "products?type=" + params.slug + "&page=" + page,
+      });
+    }
+    return response.data.products;
   } catch (error) {
     console.log(error);
     throw error;
@@ -53,11 +62,16 @@ async function getProducts(params, page = 1) {
 function listings(props) {
   const [page, setpage] = useState(1);
   const [products, setProducts] = useState(props.products);
-
+  const [haseMore, setHaseMore] = useState(true);
+  console.log(props);
   return (
     <div className={styles.container}>
       <div className={styles.head}>
-        <h3>Men's Wear (113)</h3>
+        {props.params.slug === "new" ? (
+          <h3>Most recent in the store</h3>
+        ) : (
+          <h3>{`${props.params.slug}'s Wear`}</h3>
+        )}
         <div>
           {/* <p>
             <Dropdown icon={<SortIcon />} name={"Sort"} />
@@ -68,13 +82,14 @@ function listings(props) {
       <InfiniteScroll
         dataLength={products.length}
         className={styles.cards}
-        hasMore={true}
+        hasMore={haseMore}
         next={async () => {
           const newProducts = await getProducts(props.params, page + 1);
           setProducts((prev) => [...prev, ...newProducts]);
-          console.log(products);
           if (newProducts.length !== 0) {
             setpage((prev) => prev + 1);
+          } else {
+            setHaseMore(false);
           }
         }}
       >
